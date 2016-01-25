d6b0f15a933163acfad72f6aecefe0cb7f3be560
aysin71 <aysin71@gmail.com>
2016-01-25 12:07:06 +0100

    real screen without constraints set

--- belikeMeme/belikeMeme/ViewController.swift
+++ belikeMeme/belikeMeme/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var thisIsXText: UILabel!
+    
+    @IBOutlet weak var wiseText: UILabel!
+    
+    @IBOutlet weak var beText: UILabel!
+    
+    @IBOutlet weak var beLikeXText: UILabel!
+    
+    @IBOutlet weak var billImage: UIImageView!
+
+    
+    @IBAction func changeImage(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
