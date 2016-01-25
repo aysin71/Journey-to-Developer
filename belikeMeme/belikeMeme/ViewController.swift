c2fade1ad0aadb3c928463cfa1e82ac5c487f500
aysin71 <aysin71@gmail.com>
2016-01-25 11:28:38 +0100

    Scoreboard designed may not be aligned

---
+++ belikeMeme/belikeMeme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  belikeMeme
+//
+//  Created by Aysin Kuran on 1/25/16.
+//  Copyright © 2016 Aysin Kuran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
